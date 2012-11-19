dab0066536a482a334c6c925a61aa1171ae571c4
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-11-20 03:32:23 +0900

    updated annotations

--- spec/models/inter_library_loan_spec.rb
+++ spec/models/inter_library_loan_spec.rb
@@ -10,9 +10,9 @@ end
 #
 # Table name: inter_library_loans
 #
-#  id                   :integer         not null, primary key
-#  item_id              :integer         not null
-#  borrowing_library_id :integer         not null
+#  id                   :integer          not null, primary key
+#  item_id              :integer          not null
+#  borrowing_library_id :integer          not null
 #  requested_at         :datetime
 #  shipped_at           :datetime
 #  received_at          :datetime
@@ -20,7 +20,7 @@ end
 #  return_received_at   :datetime
 #  deleted_at           :datetime
 #  state                :string(255)
-#  created_at           :datetime
-#  updated_at           :datetime
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
 #
 
