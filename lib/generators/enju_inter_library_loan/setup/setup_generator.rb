e1faa7197f7f5c8903f8bb54d904aa3217882777
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-12 02:26:33 +0900

    removed generator

--- lib/generators/enju_inter_library_loan/setup/setup_generator.rb
+++
@@ -1,7 +0,0 @@
-class EnjuInterLibraryLoan::SetupGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
-
-  def setup
-    inject_into_class 'app/models/item.rb', Item,
-      "  enju_inter_library_loan_item_model\n"
-end
