f6a646c2eb725c402128f319dd1cdb40eefb42f1
Kosuke Tanabe <kosuke@e23.jp>
2013-11-11 00:54:50 +0900

    updated gemspec

--- enju_inter_library_loan.gemspec
+++ enju_inter_library_loan.gemspec
@@ -24,6 +24,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency "factory_girl_rails"
   s.add_development_dependency "sunspot_solr", "~> 2.1"
   s.add_development_dependency "sunspot-rails-tester"
-  s.add_development_dependency "enju_leaf", "~> 1.1.0.rc6"
+  s.add_development_dependency "enju_leaf", "~> 1.1.0.rc7"
   s.add_development_dependency "enju_circulation", "~> 0.1.0.pre31"
 end
--- spec/dummy/config/initializers/devise.rb
+++ spec/dummy/config/initializers/devise.rb
@@ -1,6 +1,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.secret_key = 'd9bd25947fd700fb8d2fe808368384a0578de7c65822e81f3e8430407c8664c5bda0a11fced720e1b41979d06d0db8c96202a3c24dc536c395d7f244795cb820'
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
