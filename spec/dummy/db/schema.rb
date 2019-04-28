08eee2269d29d7517d973d6e4511d0a3905eeb2d
Kosuke Tanabe <nabeta@fastmail.fm>
2019-04-28 19:27:06 +0900

    update factory files

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180107164929) do
+ActiveRecord::Schema.define(version: 2018_01_07_164929) do
 
   create_table "accepts", force: :cascade do |t|
     t.integer "basket_id"
@@ -1231,8 +1231,9 @@ ActiveRecord::Schema.define(version: 20180107164929) do
     t.integer "user_export_file_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean "most_recent"
+    t.boolean "most_recent", null: false
     t.index ["sort_key", "user_export_file_id"], name: "index_user_export_file_transitions_on_sort_key_and_file_id", unique: true
+    t.index ["user_export_file_id", "most_recent"], name: "index_user_export_file_transitions_parent_most_recent", unique: true, where: "most_recent"
     t.index ["user_export_file_id"], name: "index_user_export_file_transitions_on_file_id"
     t.index ["user_export_file_id"], name: "index_user_export_file_transitions_on_user_export_file_id"
   end
@@ -1299,8 +1300,9 @@ ActiveRecord::Schema.define(version: 20180107164929) do
     t.integer "user_import_file_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean "most_recent"
+    t.boolean "most_recent", null: false
     t.index ["sort_key", "user_import_file_id"], name: "index_user_import_file_transitions_on_sort_key_and_file_id", unique: true
+    t.index ["user_import_file_id", "most_recent"], name: "index_user_import_file_transitions_parent_most_recent", unique: true, where: "most_recent"
     t.index ["user_import_file_id"], name: "index_user_import_file_transitions_on_user_import_file_id"
   end
 
--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     f.sequence(:item_identifier){|n| "item_#{n}"}
     f.circulation_status_id{CirculationStatus.find(1).id}
     f.manifestation_id{FactoryBot.create(:manifestation).id}
-    f.bookstore_id 1
-    f.budget_type_id 1
+    f.bookstore_id { 1 }
+    f.budget_type_id { 1 }
   end
 end
